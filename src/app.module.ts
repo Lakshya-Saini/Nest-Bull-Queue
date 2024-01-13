3aeea18b57ab2696c91ce7aeda883ed5062ce11d
Lakshya Saini <saini.lakshya1998@gmail.com>
2024-01-13 23:11:48 +0530

    registered bull queue config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    BullModule.forRoot({
+      redis: {
+        host: '172.17.0.2',
+        port: 6379,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
